6d0125eb62b88c93b126047763b89f8932de5b62
Tomas <thomasnguen2000@gmail.com>
2026-06-29 23:58:56 +0200

    Add contact page

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean, Text
+import os
+import dotenv
+import smtplib
+from email.message import EmailMessage
+
+dotenv.load_dotenv()
+
+MY_EMAIL = os.environ.get("MY_EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
 
 app = Flask(__name__)
 
@@ -34,6 +43,7 @@ class Skill(db.Model):
 with app.app_context():
     db.create_all()
 
+
 # new_project = Project(name="Jo Code Translator",
 #                       description="A Python command-line application that translates text to Morse code and vice versa.",
 #                       image="projects/morse.png", github_link="https://github.com/NugyTomas/morse-code-translator",
@@ -55,15 +65,42 @@ def inject_year():
         "current_year": datetime.now().year
     }
 
+
 @app.route('/')
 def home():
     projects = db.session.execute(db.select(Project)).scalars().all()
     skills = db.session.execute(db.select(Skill)).scalars().all()
     return render_template("index.html", navbar="contact", projects=projects, skills=skills)
 
-@app.route('/contact')
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        data = request.form
+        send_email(data)
+        return render_template("contact.html", navbar="home")
     return render_template("contact.html", navbar="home")
+
+
+def send_email(data):
+    email = EmailMessage()
+    email["Subject"] = "New message from TNG website"
+    email["From"] = MY_EMAIL
+    email["To"] = MY_EMAIL
+
+    email.set_content(
+        f"""
+Name: {data['name']}
+Email: {data['email']}
+
+Message:
+{data['message']}
+"""
+    )
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.send_message(email)
 
 
 if __name__ == "__main__":
